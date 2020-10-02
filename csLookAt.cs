17207d7584b5f305db502846af6a7ee7290aab58
James Park <woghd8503@naver.com>
2020-10-02 16:18:20 +0900

    LookAt 특정한 게임오브젝트를 주시하는법.

---
+++ csLookAt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csLookAt : MonoBehaviour
+{
+    // Capital letter 대문자로 표현하는 것은 변수 타입.
+    Transform obj = null;
+
+    void Start()
+    {
+        // 찾기
+        obj = GameObject.Find("Cube2").transform; // Small letter 모든 컴포넌트는 소문자로 표현.
+    }
+   
+    void Update()
+    {
+        // 주시
+        transform.LookAt(obj);
+    }
+}
