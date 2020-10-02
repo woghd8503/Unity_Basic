e2cb854128b7ac154d176fa3df8dca7310d76e70
James Park <woghd8503@naver.com>
2020-10-02 15:01:05 +0900

    FixedUpdate(Rigidbody) 게임 오브젝트에 힘을 줘서 움직임을 만드는 방법.

---
+++ csThrow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csThrow : MonoBehaviour
+{
+    float power = 800.0f;
+    Vector3 velocity = new Vector3(0.5f, 0.5f, 0.0f);
+
+    // If you want to apply a foce over several frames
+    // you should apply it inside FixedUpdate instead of Update
+
+    void FixedUpdate()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<Rigidbody>().AddForce(velocity * power);
+        }
+    }
+}
