1f01251f3fa7ce8ecb9bf2292e0fa8f424432382
James Park <woghd8503@naver.com>
2020-10-02 16:29:21 +0900

    RotateAround 특정한 오브젝트를 기준으로 주변을 빙글빙글 돌게 하는 방법.

---
+++ csRotateAround.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csRotateAround : MonoBehaviour
+{
+    Transform obj = null;
+    void Start()
+    {
+        // 찾기
+        obj = GameObject.Find("Cube2").transform;
+    }
+
+    void Update()
+    {
+        // 주변 돌기 1
+        // transform.RotateAround(Vector3.zero, Vector3.up, 40 * Time.deltatime);
+
+        // 주변 돌기 2
+        transform.RotateAround(obj.position, Vector3.up, 40 * Time.deltaTime); 
+        // the first parameter is for 중심 위치, Second 회전의 중심이 될 축, Third 속도 
+
+        transform.LookAt(obj);
+    }
+}
