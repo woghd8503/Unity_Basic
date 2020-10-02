9f75b8382b15daf5ff85e77b0aaafda617864f57
James Park <woghd8503@naver.com>
2020-10-02 13:12:57 +0900

    csMove

---
+++ csMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csMove : MonoBehaviour
+{
+    void Start()
+    {
+        // 트랜스폼 포지션 세팅 : 이동
+        // 해당 Vector 의 값으로 이동하여 위치하게 됨
+        this.transform.position = new Vector3(0.0f, 0.5f, 0.0f);
+
+        // 이동
+        // 현재 위치에서 주어진 값만큼 이동
+        this.transform.Translate(new Vector3(0.0f, 0.0f, 1.0f));
+    }
+}
---
+++ csMove1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csMove1 : MonoBehaviour
+{
+    float speed = 20.0f;
+
+    void Update()
+    {
+        // 키보드 입력
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        // 이동 거리 보정
+        h = h * speed * Time.deltaTime;
+        v = v * speed * Time.deltaTime;
+
+        // 실제 이동
+        transform.Translate(Vector3.right * h);
+        transform.Translate(Vector3.forward * v);
+        
+    }
+}
