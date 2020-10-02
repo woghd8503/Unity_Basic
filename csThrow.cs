ff91279f3712f345328576c4f311f68d3e799af7
James Park <66048976+woghd8503@users.noreply.github.com>
2020-10-02 15:08:45 +0900

    Update csThrow.cs

--- csThrow.cs
+++ csThrow.cs
@@ -9,7 +9,9 @@ public class csThrow : MonoBehaviour
 
     // If you want to apply a foce over several frames
     // you should apply it inside FixedUpdate instead of Update
-
+    
+    // Update와 다르게 FixedUpdate는 Time.deltaTime 값을 곱할 필요가 없다. 이는 프레임 속도와는 독립적으로 타이머가 호출됨.
+    // 대신 Edit -> Project Setting -> Time -> Fixed Timesep 에서 값을 수정해서 원하는 속도를 조절. 
     void FixedUpdate()
     {
         if(Input.GetButtonDown("Fire1"))
