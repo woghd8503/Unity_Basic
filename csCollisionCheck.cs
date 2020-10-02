58374777dd3c5ae76e7d8ea65c9349c2104a5e1a
James Park <woghd8503@naver.com>
2020-10-02 19:48:35 +0900

    CollisionCheck 충돌을 확인하고, 충돌 시 어떤 동작을 해야 하는지 결정.

--- csCollisionCheck.cs
+++ csCollisionCheck.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* OnCollisionEnter 이벤트가 발생하는 조건
+ 1. 두 게임 오브젝트 모두 Collider가 있어야 한다.
+ 2. 둘 중 하나는 Rigidbody가 있어야 한다.
+ 3. 그리고 Rigidbody를 가진 쪽이 움지여서 서로 만났을 때만 이벤트가 발생한다.*/
 public class csCollisionCheck : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
