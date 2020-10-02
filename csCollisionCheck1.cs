0652819187ef821f7416006b67d9b2393396f603
James Park <woghd8503@naver.com>
2020-10-02 20:12:14 +0900

    Is Trigger 충돌이 일어나면 충돌한 결과를 나타내기 위해 물리 연산이 생김. 따라서 잦은 물리 연산으로 인해 성능 저하를 방지하고자 물리연산을 하지 않는 Trigger를 사용.

---
+++ csCollisionCheck1.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("OnCollisionEnter");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+    }
+}
