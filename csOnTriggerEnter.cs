918e2826c3f1015e3a688143863b740e4e37246a
James Park <woghd8503@naver.com>
2020-10-02 20:39:20 +0900

    Is Trigger 충돌이 일어나면 충돌한 결과를 나타내기 위해 물리 연산이 생김. 따라서 잦은 물리 연산으로 인해 성능 저하를 방지하고자 물리연산을 하지 않는 Trigger를 사용.

---
+++ csOnTriggerEnter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csOnTriggerEnter : MonoBehaviour
+{
+    /* OnTriggerEnter 이벤트가 발생하는 조건을 정리해 보면 다음과 같다.
+     * 1. 두 게임 오브젝트 모두 콜라이더 (Collider)가 있어야 한다.
+     * 2. 둘 중 하나는 Rigidbody가 있어야 한다.
+     * 3. 둘 중 하나는 콜라이더에 Is Trigger가 체크돼 있어야 한다.
+     * 4. 그리고 어느 쪽이 움직이더라도 서로 만나면 이벤트가 발생한다.
+     */
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+    }
+}
+
