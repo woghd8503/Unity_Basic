211f745debbe69827c69b4ab98312800b0c470d4
James Park <woghd8503@naver.com>
2020-10-02 15:19:13 +0900

    FixedUpdate(Rigidbody) 게임 오브젝트에 힘을 줘서 움직임을 만드는 방법.

---
+++ csThrow1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csThrow1 : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        // The velocity vector of the rigibody.
+        // In most cases you should not modify the velocity directly, as this can result in unrealistic behavior.
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(7, 7, 0);
+        }
+    }
+}
