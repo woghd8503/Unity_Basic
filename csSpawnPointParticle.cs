c5b2bbca51c291451c61b006d85f126894e9989c
James Park <woghd8503@naver.com>
2020-10-03 22:55:31 +0900

    SpawnPointParticle 설정한 위치에서 마우스 클릭 시 particle을 발생

---
+++ csSpawnPointParticle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Main camera에 추가 -> Spawn Point와  Explosion 변수 값을 초기화
+public class csSpawnPointParticle : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject myParticle;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            DoMyParticle();
+        }
+    }
+
+    void DoMyParticle ()
+    {
+        GameObject particleObj = Instantiate(myParticle) as GameObject;
+        particleObj.transform.position = spawnPoint.position;
+
+        Destroy(particleObj, 1.0f);
+    }
+}
