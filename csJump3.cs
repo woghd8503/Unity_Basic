2b80d3cd3c2e89c6413d82060b152b622d5994e1
James Park <woghd8503@naver.com>
2020-10-03 10:43:04 +0900

    Jump CharacterController를 사용해 자연스럽게 점프한다.

---
+++ csJump3.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csJump3 : MonoBehaviour
+{
+    float gravity = -20.0f;     // gravity
+    float jumpSpeed = 10.0f;    // jump speed
+    Vector3 velocity;           // current speed
+
+    CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        velocity = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        float y = transform.position.y;
+        if (y < 1)
+            gameObject.transform.position = new Vector3(0.0f, 1.1f, 0.0f);
+
+        // 캐릭터가 땅에 닿아 있는지 체크
+        if (controller.isGrounded)
+        {
+            if(Input.GetButtonDown("Jump"))
+            {
+                velocity.y = jumpSpeed;
+            }
+        }
+        velocity.y += (gravity * Time.deltaTime);
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
