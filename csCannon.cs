a865c18bea2c342c2c1fc758ee6dbcd53dce9255
James Park <woghd8503@naver.com>
2020-10-03 21:06:29 +0900

    Instantiate 프리팹을 이용해 game object를 동적으로 만드는 method

---
+++ csCannon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Trail Renderer
+  포탄이 날라갈 때 특수 효과를 추가. Component -> Effects -> Trail Renderer -> add defualt material*/
+public class csCannon : MonoBehaviour
+{
+    float power = 900.0f;
+    Vector3 velocity = new Vector3(0.0f, 0.3f, 0.5f);
+
+    void Start()
+    {
+        velocity = velocity * power;
+
+        GetComponent<Rigidbody>().AddForce(velocity);
+    }
+
+    void FixedUpdate()
+    {
+        if(this.transform.position.z > 10.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ csInstantiate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Main camera에 추가
+// Instantiate를 사용해 Prefab(premade fabrication)을 연속적으로 생성
+public class csInstantiate : MonoBehaviour
+{
+    public Transform firePos;
+    public GameObject cannon;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(cannon, firePos.position, firePos.rotation); // what, where, where
+        }
+    }
+}
