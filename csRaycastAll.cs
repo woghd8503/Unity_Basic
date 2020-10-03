6de8a00cb702e306ef9afe83048744ae732cb0e6
James Park <woghd8503@naver.com>
2020-10-03 23:41:00 +0900

    hits = Physics.RaycastAll(transform.position, transform.forward, 8.0f); 충돌한 모든 object이름을 출력

---
+++ csRaycastAll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csRaycastAll : MonoBehaviour
+{
+    private float speed = 5.0f;
+    void Update()
+    {
+        // 이동
+        float amtmove = speed * Time.deltaTime;
+        float hor = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hor * amtmove);
+
+        // DrawRay
+        Debug.DrawRay(transform.position, transform.forward * 8, Color.red);
+
+        // RaycastAll
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, 8.0f);
+
+        // 광선이 충돌한 게임 오브젝트의 이름을 콘솔 창에 전부 출력한다.
+        for(int i = 0; i< hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+}
