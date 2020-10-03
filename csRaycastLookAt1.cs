81bd62b9f3ee3534ff073cb6a0ef9f58ac4023a4
James Park <woghd8503@naver.com>
2020-10-04 00:40:44 +0900

    viewer를 이용해서 Turret이 바닥 쪽으로 처지는 것을 해결

---
+++ csRaycastLookAt1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csRaycastLookAt1 : MonoBehaviour
+{
+    public Transform enemy;
+    private Transform viewer;
+    private Transform spPoint;
+
+    RaycastHit hit;
+    Vector3 fwd = Vector3.forward;
+    void Start()
+    {
+        viewer = transform.Find("/Turret/Viewer");
+        spPoint = transform.Find("/Turret/Tower/SpawnPoint");
+    }
+
+    void Update()
+    {
+//      transform.LookAt(enemy);
+        viewer.LookAt(enemy);
+        transform.rotation = viewer.rotation;
+
+        Debug.DrawRay(spPoint.position, spPoint.forward * 4, Color.red);
+
+        if(Physics.Raycast(spPoint.position, fwd, out hit, 4))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+}
