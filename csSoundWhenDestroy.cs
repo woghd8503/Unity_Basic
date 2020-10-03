02ce09852d8d5ae2c5a33ad4127abdf3b2ffa708
James Park <woghd8503@naver.com>
2020-10-03 15:08:33 +0900

    Destroy(this.gameObject); 충돌과 동시에 Play() 메서드와 Destroy메서드가 실행 따라서 소리 축력없이 게임오브젝트가 삭제

---
+++ csSoundWhenDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csSoundWhenDestroy : MonoBehaviour
+{
+    /*Play() 메서드는 오디오 소스 컴포넌트의 audioClilp을 출력
+      PlayOneShot(audioClip) 메서드는 오디오 소스 컴포넌트 외부에 있는 오디오 클립을 출력*/
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<AudioSource>().Play();
+
+        // 지정한 게임 오브젝트를 게임에서 제거한다.
+        Destroy(this.gameObject);
+    }
+}
+
