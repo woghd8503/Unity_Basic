7eb12ebc5f47f7c7696949da54c9b32877554c72
James Park <woghd8503@naver.com>
2020-10-03 16:12:14 +0900

    3DSound Spatial Blend값을 1로 설정 Audio listener와 game object의 거리에 따라 소리의 크기가 변화

---
+++ csSondCheck3DSound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// <3D Sound Settings>
+// Spatial Blend 1으로 설정 -> Volume Rolloff는 Linear Rolloff설정 -> Min Distance 5 ~ Max Distance 10
+public class csSondCheck3DSound : MonoBehaviour
+{
+    float speed = 20.0f;
+
+    void Update()
+    {
+        // 키보드 입력
+        float v = Input.GetAxis("Vertical");
+
+        // 이동거리 보정
+        v = v * speed * Time.deltaTime;
+
+        // 실제 이동
+        transform.Translate(Vector3.forward * v);
+
+        // Play 3D Sound Check
+        // In Audio Source, Change Spatial Blend. (2d - 3d)
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
