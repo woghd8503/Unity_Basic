69c9c8fab74c8c4a2d0d2701249295465a7272ff
James Park <woghd8503@naver.com>
2020-10-04 00:09:36 +0900

    LookEnemy 적의정보를 저장, 적을 주시하면서 적의 움직임에 따라 회전

---
+++ csRaycastLookAt.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csRaycastLookAt : MonoBehaviour
+{
+    public Transform enemy;
+    private Transform spPoint;
+
+    RaycastHit hit;
+    Vector3 fwd = Vector3.forward;
+    void Start()
+    {
+        spPoint = transform.Find("/Turret/Tower/SpawnPoint");
+    }
+
+    void Update()
+    {
+        transform.LookAt(enemy);
+
+        Debug.DrawRay(spPoint.position, spPoint.forward * 4, Color.red);
+
+        if(Physics.Raycast(spPoint.position, fwd, out hit, 5))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+}
