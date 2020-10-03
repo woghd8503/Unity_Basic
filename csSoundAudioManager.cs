482fb2b1370637cce2331e4eebd8906ac4144886
James Park <woghd8503@naver.com>
2020-10-03 15:45:01 +0900

    AudioManager (Sington pattern) AudioManager는 maincamera에 추가, Audioplay 는 gameobject

---
+++ csSoundAudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 소프트웨어 디자인 패턴 중에 singleton이라는 디자인 패턴이 있다. 게임 개발에 많이 사용하는 패턴으로
+// 어떤 클래스에 대해 하나의 객체만 생성하고 싶을 대 사용한다. static 변수를 활용해 성생하여,
+// 메모리에 늘 하나의 객체만 상주하는 특징이 있다.
+
+/* AudioManager를 MainCamera 또는 empty object(Manager)에 추가한다. 절대 제거되지 않을 곳에 추가한다. 그리고 audiosource component를 추가*/
+
+public class csSoundAudioManager : MonoBehaviour
+{
+    static csSoundAudioManager _instance = null;
+    public static csSoundAudioManager Instance()
+    {
+        return _instance;
+    }
+
+    private void Start()
+    {
+        if (_instance == null)
+            _instance = this;
+    }
+
+    public void PlaySfx(AudioClip clip)
+    {
+        GetComponent<AudioSource>().PlayOneShot(clip);
+    }
+}
---
+++ csSoundAudioPlay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csSoundAudioPlay : MonoBehaviour
+{
+    public AudioClip clip;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // 오디오 매니저 싱글톤 객체를 가져온 뒤 싱글톤 객체의 PlaySfx 메서드를 호출해 사운드를 출력한다.
+        csSoundAudioManager.Instance().PlaySfx(clip);
+
+        Destroy(this.gameObject);
+    }
+}
