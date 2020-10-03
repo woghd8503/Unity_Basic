144bfdfbb5e00dd7c0a748c0736c1fa187d849ab
James Park <woghd8503@naver.com>
2020-10-04 01:54:41 +0900

    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);클릭한 곳으로 물체를 이동

---
+++ csRaycastScreenPointToRay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csRaycastScreenPointToRay : MonoBehaviour
+{
+
+    void Update()
+    {
+        if(Input.GetButtonUp("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit)) // 12번 줄에서 구한 레이 정보로 레이캐스팅
+            {
+                Vector3 newPos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                transform.position = newPos;  // 새로운 벡터값의 위치로 탱크를 이동
+            }
+        }
+    }
+}
