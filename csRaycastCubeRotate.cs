7b465fa9c4d97f1e1ed5ab3cf8287df2eb724207
James Park <woghd8503@naver.com>
2020-10-04 02:18:58 +0900

    if (hit.transform.tag.Equals("Enemy")) 화면에 있는 적을 클릭하면 적(Tag = enemy)을 회전

---
+++ csRaycastCubeRotate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// gameobject에 추가한다.
+public class csRaycastCubeRotate : MonoBehaviour
+{
+    float accTime = 0.0f;
+    bool bRotate = false;
+
+    void Update()
+    {
+        if(accTime > 1.0)
+        {
+            bRotate = false;
+        }
+
+        if(bRotate)
+        {
+            accTime += Time.deltaTime;
+
+            transform.Rotate(100.0f * Time.deltaTime * Vector3.up);
+        }
+    }
+
+    public void RotateByHit()
+    {
+        accTime = 0.0f;
+        bRotate = true;
+    }
+}
---
+++ csScreenPointTouch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// main camera에 추가
+
+/* Raycast는 3D 공간인 월드에서 게임 오브젝트 사이의 충돌을 체크하는 데 사용
+   ScreenPointToRay는 화면을 터치해 게임 안의 게임 오브젝트를 선택하는 데 사용*/
+public class csScreenPointTouch : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) // 레이케스트 충돌체를 찾음
+            {
+                if (hit.transform.tag.Equals("Enemy"))
+                {
+                    csRaycastCubeRotate cubeScript = hit.transform.GetComponent<csRaycastCubeRotate>(); 
+                    // public으로 선언된 외부 게임 오브젝트의 스크립트에 대한 변수를 선언하고 값을 연결
+                    if (cubeScript != null)
+                    {
+                        cubeScript.RotateByHit(); // 18번 줄에서 연결한 외부 스크립트의 퍼블릭 메서드를 호출
+                    }
+                }
+            }
+        }
+    }
+}
