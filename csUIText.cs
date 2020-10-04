50ed8bc9453abee7bb6748952c927cc09bfce3d5
James Park <woghd8503@naver.com>
2020-10-04 22:14:06 +0900

    UIManager에 스크립트를 넣어서 text내용을 바꿈

---
+++ csUIText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // 추가
+
+// 빈 게임오브젝트를 UImanager로 이름을 변경 후 this script를 추가
+public class csUIText : MonoBehaviour
+{
+    GameObject obj;
+    Text txt;
+
+    void Start()
+    {
+        obj = GameObject.Find("txtCenter");
+        txt = obj.GetComponent<Text>();
+
+        ChangeText();
+    }
+
+    void ChangeText()
+    {
+        txt.text = "홍길동";
+    }
+}
