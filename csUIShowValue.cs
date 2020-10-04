034244de7c24d17ca1c374cfb10ff382ea8779b1
James Park <woghd8503@naver.com>
2020-10-05 01:31:35 +0900

    Slider handle을 마우스로 drag하면 다른 slider와 Text %도 같이 변화.

---
+++ csUIShowValue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;    //추가
+
+// Slider1 자식의 Label Percent (UI text)에 this script를 추가
+// Slider1 component에서 On Value Changed에서 Label Percent를 추가 후 options 위쪽에 있는 UpdateLabel을 호출
+// 숫자로 percentage를 나타나기 위해 만든 script
+public class csUIShowValue : MonoBehaviour
+{
+    public void UpdateLabel(float value)
+    {
+        Text lbl = GetComponent<Text>();
+        if(lbl != null)
+        {   // 파라미터로 넘어온 값을 이용 수학함수를 이용하여 정수로 만들어 Text UI의 값에 대입
+            lbl.text = Mathf.RoundToInt(value * 10) + "%";  
+        }
+    }
+}
---
+++ csUISlider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;       // 추가
+
+// 빈오브젝트를 UIManager로 이름을 변경, this script를 추가
+// slider2 conponent에서 On Value Changed추가 ChangeSliderValue 메서드를 호출
+public class csUISlider : MonoBehaviour
+{
+    Text txt;
+    Slider slider1;
+    Slider slider2;
+    int fontSize;
+
+    void Start()
+    {
+        txt = GameObject.Find("txtCenter").GetComponent<Text>();
+        slider1 = GameObject.Find("Slider1").GetComponent<Slider>();
+        slider2 = GameObject.Find("Slider2").GetComponent<Slider>();
+
+        fontSize = txt.fontSize;
+    }
+
+    public void ChangeSliderValue()
+    {
+        float val = slider2.value;
+        Debug.Log("slider2.value : " + val);
+
+        slider1.value = val;
+        txt.fontSize = fontSize + (int)val; // 현재 폰트 사이즈에 슬라이더의 값을 더하여 폰트 사이즈를 늘리거나 줄인다.
+    }
+}
+
