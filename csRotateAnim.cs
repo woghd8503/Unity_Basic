b9856988d35d75d6b844832363405ed4b5f4ac7c
James Park <woghd8503@naver.com>
2020-10-05 07:04:27 +0900

    큐브를 좌우 회전, 일정 키프레임에 따라 renderer가 변화, 마우스 클릭시 움직임

---
+++ csRotateAnim.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csRotateAnim : MonoBehaviour
+{
+    public Material mat1;
+    public Material mat2;
+    Animator anim;
+
+    void FrontImage()
+    {
+        //GetComponent<Renderer>().material.mainTexture = Resources.Load("image1");
+        GetComponent<Renderer>().material = mat1;
+    }
+    void BackImage()
+    {
+        //GetComponent<Renderer>().material.mainTexture = Resources.Load("image2");
+        GetComponent<Renderer>().material = mat2;
+    }
+
+    // addition
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnMouseUp()
+    {
+        StartCoroutine("coAnim");
+    }
+
+    IEnumerator coAnim()
+    {
+        anim.SetInteger("aniStep", 1);
+        yield return new WaitForSeconds(1.0f);
+        anim.SetInteger("aniStep", 0);
+    }
+}
