c7ac400b709af5040307e93a774a68b468d62227
James Park <woghd8503@naver.com>
2020-10-05 02:22:57 +0900

    ScrollBar 의 움직임에 따라 Text UI의 값이 조절된다

---
+++ csUIScrollBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;    // 추가 
+
+// Text txtHor / txtVer 에 this script를 추가 하고 scrollbar에 On Value Changed에 txtHor / txtVer를 추가 상위에 있는 UpdateLabel을 tick
+public class csUIScrollBar : MonoBehaviour
+{
+    Text lbl;
+
+    void Start()
+    {
+        lbl = GetComponent<Text>();
+    }
+
+    public void UpdateLabel(float value)                 // 사용자 정의 메서드를 만든다. 스크롤바의 움직임 값을 파라미터로 받을 것이다. 
+    {
+        if (lbl != null)
+            lbl.text = string.Format("{0:0.00}", value); // 스크롤바의 움직임 값을 Text UI의 text 값에 대입해 준다.
+    }
+}
