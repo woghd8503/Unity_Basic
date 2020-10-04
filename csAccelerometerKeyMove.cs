78cedb8f42f18fd34433485ff4a30aa0b33cc335
James Park <woghd8503@naver.com>
2020-10-04 10:55:09 +0900

    Accelerometer key move 방향키를 이용해 object를 좌우 앞뒤로 기울어짐

---
+++ csAccelerometerKeyMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csAccelerometerKeyMove : MonoBehaviour
+{
+    void Update()
+    {
+        // Horizontal - Z axis
+        transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Horizontal") * 30.0f * -1 * Time.deltaTime, Vector3.forward);
+
+        // Vertical - X axis
+        transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Vertical") * 30.0f * -1 * Time.deltaTime, Vector3.left);
+    }
+}
