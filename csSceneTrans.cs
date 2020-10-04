020bcf2040cb1a803967ff66a71c31f39d84eb42
James Park <woghd8503@naver.com>
2020-10-04 15:59:04 +0900

    SceneTrans Scene과 Scene을 전환하는 방법

---
+++ csSceneTrans.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //  SceneManager 추가
+
+// 전환할 두개의 Scenes을 build setting에 add Open Scenes에 추가
+// This script를 main camera에 추가
+// button에 On Click에 main camera에 있는 method호출 설정
+public class csSceneTrans : MonoBehaviour
+{
+    public void SceneTrans1_1()
+    {
+        SceneManager.LoadScene("17.LoadSceneEx");
+    }
+
+    public void SceneTrans1_2()
+    {
+        SceneManager.LoadScene("17.LoadSceneEx 1");
+    }
+}
