085845e2b9a840f51fc5e06c644742efccc01623
James Park <woghd8503@naver.com>
2020-10-04 22:22:32 +0900

    UI Button 버튼 눌렀을 시 public void ChangeText method를 호출

---
+++ csUIButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;       //추가
+
+// 스크립트가 든 main camera or empty object(UIManager)를 button On Click에 추가
+// 버튼 눌렀을 시 public void ChangeText method를 호출
+
+public class csUIButton : MonoBehaviour
+{
+    GameObject obj;
+    Text txt;
+
+    void Start()
+    {
+        obj = GameObject.Find("txtCenter");
+        txt = obj.GetComponent<Text>();
+    }
+    public void ChangeText()
+    {
+        txt.text = "홍길동";
+    }
+}
